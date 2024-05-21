9241e46e657d77cccac300d29e5793b19a21ae60
tehminashoaib <tehminashoaib61@gmail.com>
2024-05-21 12:01:00 -0700

    first currency convertor

---
+++ index.ts
@@ -0,0 +1,58 @@
+import inquirer from "inquirer"
+import {log} from "console";
+
+let currencyconvertor ={
+    "USD" :{
+        "USD":1,
+        "EUR":0.92,
+        "PKR":278
+    },
+    "EUR" :{
+        "USD":1.08,
+        "EUR":1,
+        "PKR":300
+    },
+    "PKR" :{
+        "USD":0.0036,
+        "EUR":0.0033,
+        "PKR":1
+    },
+}
+
+//INPUT PROMPT
+
+const answer: {
+    from:"USD" |"EUR" |"PKR",
+    to: "USD" |"EUR" |"PKR",
+    amount : number,
+} = await inquirer.prompt(
+    [
+        {
+            name: "from",
+            message: "Enter your currency which you want to convert?",
+            type: "list",
+            choices: ["USD", "EUR", "PKR"]
+        },
+        {
+            name: "to",
+            message: "Select which currency conver?",
+            type: "list",
+            choices: ["USD", "EUR", "PKR"]
+        },
+        {
+            name: "amount",
+            message: "Enter your amount in number",
+            type: "number",
+            choices: ["USD", "EUR", "PKR"]
+        }
+    ]
+)
+const {from,to,amount} = answer;
+
+//conversion
+if(from && to && amount){
+    let result =currencyconvertor[from][to] * amount;
+    console.log(`your conversion from ${from} to ${to} is ${result}`)
+}else {
+    console.log("invalid operator select")
+}
